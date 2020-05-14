3593ae198ea81617838d5f16bf79af8134b0bdd0
Bruno Madeira <brunoam58@gmail.com>
2020-05-14 19:13:41 -0300

    consumo

---
+++ consumo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace consumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int dist;
+            double consumo, gasto;
+
+            Console.WriteLine("Distancia percorrida: ");  
+            dist = int.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Combustivel gasto: ");
+            gasto = double.Parse(Console.ReadLine(), CI);  
+
+            consumo = dist / gasto;
+
+            Console.WriteLine("Consumo medio = "+consumo.ToString("f3", CI));
+                 
+
+
+        }
+    }
+}
